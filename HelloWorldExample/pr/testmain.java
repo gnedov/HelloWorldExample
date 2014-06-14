b51354da76cbc267e4f129cf5dc6be484d43526c
gnedov <andrey.gnedov@gmail.com>
2014-06-14 20:14:44 +0300

    copy to git repository HelloWorld project

---
+++ HelloWorldExample/pr/testmain.java
@@ -0,0 +1,14 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Andrey
+ * Date: 14/06/14
+ * Time: 00:43
+ * To change this template use File | Settings | File Templates.
+ */
+//include java.lang.* ;
+
+public class testmain {
+    public static void main(String [] arg){
+        System.out.println("wewerwer");
+    }
+}
